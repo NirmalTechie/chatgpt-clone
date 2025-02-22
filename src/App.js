ced50e51f25585761167962a7f4a63fc852c6d62
NirmalTechie <nirmalofficial22@gmail.com>
2025-02-22 23:48:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Chat from "./chat";
+
+function App() {
+    return (
+        <div className="App flex justify-center items-center h-screen bg-gray-200">
+            <Chat />
+        </div>
+    );
+}
+
+export default App;
